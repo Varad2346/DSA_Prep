7bebe32272017a4eb392b6dc6ccb604458b36d36
Varad2346 <varadlimbkar@gmail.com>
2025-02-17 11:48:40 +0530

    sliding window

---
+++ recursion/CombinationSumI.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void findCombinations(int ind,int target,vector<int>&arr,vector<vector<int>>&ans,vector<int>&ds){
+    if(ind==arr.size()){
+        if(target==0){
+            ans.push_back(ds);
+        }
+        return;
+    }
+
+    //pick up element
+    if(arr[ind]<=target){
+        ds.push_back(arr[ind]);
+        findCombinations(ind,target-arr[ind],arr,ans,ds);
+        ds.pop_back();
+    }
+    findCombinations(ind+1,target,arr,ans,ds);
+}
+
+vector<vector<int>>combinationSum(vector<int>&candidates,int target){
+    vector<vector<int>>ans;
+    vector<int>ds;
+    findCombinations(0,target,candidates,ans,ds);
+    return ans;
+}
+
+int main(){
+    vector<int>candidates={3,2,5};
+    vector<vector<int>>ans=combinationSum(candidates,5);
+    for(auto it:ans){
+        for(auto num:it){
+            cout<<num;
+        }
+        cout<<endl;
+    }
+}
